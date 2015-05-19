06d504566acb515867cbf75eae7357af7d8553dc
Daria Zolotko <nalune@gmail.com>
2015-05-18 18:09:59 -0700

    added script.js

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt)
 {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
         grunt.loadNpmTasks('grunt-contrib-watch');
+        grunt.loadNpmTasks('grunt-contrib-compass');
 	
 	grunt.initConfig({
 		uglify:
@@ -16,6 +17,17 @@ module.exports = function(grunt)
 				}//files
 			}//target
 		}, //uglify
+                
+                compass: {
+                    dev: {
+                        options:
+                                {
+                                    config: 'config.rb'
+                                }// options
+                    }// dev
+                    
+                }, // compass
+                
                 watch:
                         {   
                             options: {livereload : true}, 
@@ -24,6 +36,11 @@ module.exports = function(grunt)
                                          files: ['_/components/js/*.js'], 
                                          tasks: ['uglify']
                                     },    //scripts
+                            sass: {
+                                files: ['_/components/sass/*.scss'], 
+                                tasks: ['compass:dev']
+                                
+                            }, //sass
                             html:
                                     {
                                         files: ['*.html']
